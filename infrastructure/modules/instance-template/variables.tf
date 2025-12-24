942cefa8d34332dbbe514dab121a37c04b57d851
Deqode <deqode@Deqodes-MacBook-Pro.local>
2025-12-24 19:35:44 +0530

    c9

--- infrastructure/modules/instance-template/variables.tf
+++ infrastructure/modules/instance-template/variables.tf
@@ -3,7 +3,7 @@ variable "source_image" {}
 variable "disk_size_gb" { default = 50 }
 variable "network" {}
 variable "subnet" {}
-variable "service_account_email" {}
+
 variable "network_tags" { type = list(string)  }
 variable "metadata"    { type = map(string) }
 variable "name_prefix" { type = string }
