e11c5ff6b0f2807ff10713c684c590cdceb1fd82
disha-chhabra-0002 <dchhabra@deqode.com>
2026-01-02 19:14:34 +0530

    added startup script to application vm

--- infrastructure/modules/compute/main.tf
+++ infrastructure/modules/compute/main.tf
@@ -17,6 +17,21 @@ resource "google_compute_instance" "this" {
 
   tags = var.tags
 
-  metadata_startup_script = file("${path.module}/scripts/app-startup.sh")
+    metadata_startup_script = <<-EOF
+    #!/bin/bash
+    
+    # update packages
+    apt update -y
+    
+    # install nginx as a simple app
+    apt install -y nginx
+    
+    # write a simple index page
+    echo "<html><body><h1>Hello from GCP VM!</h1></body></html>" > /var/www/html/index.nginx-debian.html
+    
+    # start nginx
+    systemctl enable nginx
+    systemctl start nginx
+  EOF
   
 }
